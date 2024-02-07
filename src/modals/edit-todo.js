41259facbe36fd8a8b0f973da30c7d8203961296
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-02-07 15:46:57 +0300

    create edit-todo functions

--- src/modals/edit-todo.js
+++ src/modals/edit-todo.js
@@ -1,11 +1,45 @@
-import { useSelector, UseSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { editTodo } from "../stores/todo";
 
 function EditTodo({ data, close }) {
+  const dispatch = useDispatch();
+  const [todo, setTodo] = useState(data.title);
+  const [done, setDone] = useState(data.done);
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    dispatch(
+      editTodo({
+        id: data.id,
+        title: todo,
+        done,
+      })
+    );
+    close();
+  };
+
   return (
     <div>
-      edit todo modalı <br />
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <button onClick={close}>Kapat</button>
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />{" "}
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            checked={done}
+            onChange={(e) => setDone(e.target.checked)}
+          />
+          Tamamladı olarak işaretle
+        </label>{" "}
+        <br />
+        <button type="submit">Kaydet</button>
+      </form>
+      <button onClick={close}>Kapat</button> <br />
     </div>
   );
 }
--- src/stores/todo.js
+++ src/stores/todo.js
@@ -14,8 +14,16 @@ const todos = createSlice({
     deleteTodo: (state, action) => {
       state.todos = state.todos.filter !== action.payload;
     },
+    editTodo: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (action.payload.id === todo.id) {
+          todo.title = action.payload.title;
+          todo.done = action.payload.done;
+        }
+      });
+    },
   },
 });
 
-export const { addTodo, deleteTodo } = todos.actions;
+export const { addTodo, deleteTodo, editTodo } = todos.actions;
 export default todos.reducer;
